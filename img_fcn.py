658a09b079a8f0871cfc291f2ab1b9a2760c3b82
Monika <pulcovamon@gmail.com>
2023-01-03 03:24:30 +0100

    command line args

--- img_fcn.py
+++ img_fcn.py
@@ -4,22 +4,26 @@ import matplotlib.pyplot as plt
 from scipy import ndimage as ndi
 import json
 
-from skimage import filters, morphology
+from skimage import filters, morphology, img_as_ubyte
 from skimage.io import imread
 from skimage.filters import threshold_otsu
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, gray2rgb
 from skimage.segmentation import watershed
 from skimage.feature import peak_local_max, canny
 from skimage.transform import (
-    rescale, hough_circle, hough_circle_peaks, hough_line
+    rescale, hough_circle, hough_circle_peaks, hough_ellipse
 )
 from skimage.measure import regionprops_table
+from skimage.draw import ellipse_perimeter
 import json
 import os
+import re
+import argparse
 
 # () /
 
-def load_inputs(img_path, json_path):
+
+def load_inputs(img_path):
     """Function for loading image names, scales from microscope and types of particles
 
     Args:
@@ -29,6 +33,10 @@ def load_inputs(img_path, json_path):
     Returns:
         dict: dictionary with path to images, scales and types
     """
+    for root, dirs, files in os.walk(img_path):
+        for file in files:
+            if file.endswith('.json'):
+                json_path = img_path + '/' + file
 
     with open(json_path) as json_file:
         input_description = json.load(json_file)
@@ -38,6 +46,8 @@ def load_inputs(img_path, json_path):
                 os.path.join(img_path, key))
 
     return input_description
+
+
 
 
 def loading_img(img_path, scale):
@@ -53,20 +63,20 @@ def loading_img(img_path, scale):
     """
 
     img_raw = imread(img_path)
+    width = img_raw.shape[1]
 
-    line = img_raw[-100:, :]
-    line = rgb2gray(line)
-    thresh = threshold_otsu(line)
-    line = line > thresh
-    tested_angles = np.linspace(-np.pi / 2,
-                np.pi / 2, 360, endpoint=False)
-    _, _, distance = hough_line(line, theta=tested_angles)
-    length = distance[-1] - distance[0]
-    pixel_size = scale / length
+    line = img_raw[-100:, int(width/2):, :]
+    line = cv2.cvtColor(line, cv2.COLOR_RGB2GRAY)
+    line = cv2.Canny(line, 0, 255)
+    lines = cv2.HoughLinesP(line, 1, np.pi/180, threshold=50,
+                            minLineLength=100, maxLineGap=10)
+    lengths = [item[0][2]-item[0][0] for item in lines]
+    pixel_size = scale / max(lengths)
 
     img_raw = img_raw[0:-100, :]
 
     return img_raw, pixel_size
+
 
 
 
@@ -100,6 +110,7 @@ def filtering_img(img_raw, scale, type, pixel_size):
 
 
 
+
 def watershed_transform(img_filtered):
     """Function for labeling of image using calculated 
     distance map and markers for watershed transform
@@ -128,7 +139,9 @@ def watershed_transform(img_filtered):
     return labels, distance
 
 
-def hough_segmentation(img_filtered, np_type):
+
+
+def hough_segmentation(img_filtered, pixel_size):
     """Segmentation using hough circle and elipse transform
 
     Args:
@@ -141,60 +154,106 @@ def hough_segmentation(img_filtered, np_type):
 
     canny_edge = canny(img_filtered, sigma = 2)
 
-    hough_radii = np.arange(10, 100, 2)
+    start = max(10, 5/pixel_size)
+    end = min(100, 50/pixel_size)
+    hough_radii = np.arange(start, end, 2)
     hough_res = hough_circle(canny_edge, hough_radii)
 
     accums, x, y, r = hough_circle_peaks(hough_res, hough_radii,
-            min_xdistance=10, min_ydistance=10)
+                                min_xdistance=int(10),
+                                min_ydistance=int(10))
     x = np.uint16(np.around(x)) 
     y = np.uint16(np.around(y))
     r = np.uint16(np.around(r))
 
-    circles = [(x[i], y[i], r[i]) for i in range(len(x)) if accums[i] > 0.3]
+    circles = [(x[i], y[i], r[i]) for i in range(len(x)) if accums[i] > 0.35]
 
     img = np.zeros((img_filtered.shape[0], img_filtered.shape[1], 3))
     img[:, :, 0] = img_filtered
     img[:, :, 1] = img_filtered
     img[:, :, 2] = img_filtered
 
+    thresh = threshold_otsu(img_filtered)
+
     for x, y, r in circles:
-        cv2.circle(img, (x, y), r, (0, 255, 0), 1)
-        cv2.circle(img, (x, y), 1, (0, 255, 0), 2)
+        if img_filtered[x, y] > thresh:
+            circles.remove((x, y, r))
+        else:
+            cv2.circle(img, (x, y), r, (0, 255, 0), 1)
+            cv2.circle(img, (x, y), 1, (0, 255, 0), 2)
 
     return img, circles
 
 
-def ploting_img(img_median, binary, distance, labels):
-    """Function for plotting images from segmentation proces
 
-    Args:
-        img_raw (numpy.ndarray): RGB image
-        sobel_edges (numpy.ndarray): grayscale image of edges
-        region_fill (_numpy.ndarray): binary image
-        labels (numpy.ndarray): labeled image
-    """
 
-    plt.subplot(2, 2, 1)
-    plt.imshow(img_median,cmap = 'gray')
-    plt.title('Raw image')
+def hough_nanorods(img_filtered, pixel_size):
 
-    plt.subplot(2, 2, 2)
-    plt.imshow(binary, cmap = 'gray')
-    plt.title('Binary')
+    edges = canny(img_filtered, sigma=2)
+    image_rgb = np.zeros((img_filtered.shape[0], img_filtered.shape[1], 3))
+    image_rgb[:, :, 0] = img_filtered
+    image_rgb[:, :, 1] = img_filtered
+    image_rgb[:, :, 2] = img_filtered
+    
+    result = hough_ellipse(edges, accuracy=50, threshold=150,
+                       min_size=400, max_size=500)
+    result.sort(order='accumulator')
 
-    plt.subplot(2, 2, 3)
-    plt.imshow(distance, cmap = 'gray')
-    plt.title('Distance')
+    # Estimated parameters for the ellipse
+    best = list(result[-1])
+    yc, xc, a, b = [int(round(x)) for x in best[1:5]]
+    orientation = best[5]
 
-    plt.subplot(2, 2, 4)
-    plt.imshow(labels, cmap = 'tab20')
-    plt.title('Labeled image')
+    # Draw the ellipse on the original image
+    cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
+    image_rgb[cy, cx] = (0, 0, 255)
+    # Draw the edge (white) and the resulting ellipse (red)
+    edges = gray2rgb(img_as_ubyte(edges))
+    edges[cy, cx] = (250, 0, 0)
+
+    fig2, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(8, 4),
+                                    sharex=True, sharey=True)
+
+    ax1.set_title('Original picture')
+    ax1.imshow(image_rgb)
+
+    ax2.set_title('Edge (white) and result (red)')
+    ax2.imshow(edges)
 
     plt.show()
 
 
-def saving_img(img, directory = '/results/labels.png'):
-    """Function for saving labeled image into given directory.
+
+
+def ploting_img(images, names, method):
+    """Function for plotting images from segmentation proces
+
+    Args:
+        img (numpy.ndarray): image for plotting
+        file_name (str): path to image
+        method (str): segmentation method
+        number (int): number of images
+        current_num (int): number of this image
+    """
+    for i in range(len(images)):
+        width = round(len(images)/2) + 1
+        name = re.split('/|\.', names[i])
+
+        plt.subplot(2, width, i+1)
+
+        if method == 'watershed':
+            plt.imshow(images[i], cmap='tab20')
+        else:
+            plt.imshow(images[i])
+
+        plt.title(name[-2])
+    plt.show()
+
+
+
+
+def saving_img(img, file_name, directory = 'results'):
+    """Function for saving result image into given directory.
 
     Args:
         img (numpy array): labeled image
@@ -203,20 +262,19 @@ def saving_img(img, directory = '/results/labels.png'):
     Returns:
         string: path to file
     """
+    name = re.split('/', file_name)
+    res_path = directory + '/' + name[-1]
 
     ind = np.argwhere(img == 0)
     color_map = plt.get_cmap('hsv', lut = (np.amax(img)+1))
     img = color_map(img)
     img = img*255
-    img[ind[:, 0], ind[:, 1], 3] = 0
-    cv2.imwrite('results/labels_transparent.png', img)
     img[ind[:, 0], ind[:, 1], :] = 0
-    img[:, :, 3] = 255
-    cv2.imwrite(directory, img)
+    cv2.imwrite(res_path, img)
 
-    filename = directory
+    return res_path
 
-    return filename
+
 
 
 def calculation_watershed(labeled, pixel_size, np_type):
@@ -246,6 +304,8 @@ def calculation_watershed(labeled, pixel_size, np_type):
                         props[key_val][i] *= pixel_size
                     txt_file.write(str(props[key_val][i])+' ')
 
+        sizes = 2*(props['area_convex']/np.pi)**2
+
 
 
     if np_type.lower() == 'nanorods':
@@ -264,10 +324,14 @@ def calculation_watershed(labeled, pixel_size, np_type):
                         props[key_val][i] *= pixel_size
                     txt_file.write(str(props[key_val][i])+' ')
 
+        sizes = [[props['axis_major_length']], [props['axis_minor_length']]]
 
-    avg = round(sum(props['area_convex']) / len(props['area_convex']), 4)
 
-    return avg
+    avg = sum(props['area_convex']) / len(props['area_convex'])
+    diameter = 2*(avg/np.pi)**(1/2)
+
+    return diameter, sizes
+
 
 
 def calcultaion_hough_transform(circles, pixel_size, np_type):
@@ -279,38 +343,82 @@ def calcultaion_hough_transform(circles, pixel_size, np_type):
         np_type (string): nanoparticles or nanorods
 
     Returns:
-        float, float: mean radius and mean area of NP
+        float, float: mean diameter and mean area of NP
     """
 
-    radius = [r*pixel_size for x, y, r in circles]
+    diameter = [r*2*pixel_size for x, y, r in circles]
     area = [r*pixel_size*np.pi**2 for x, y, r in circles]
 
-    mean_radius = np.mean(radius)
-    mean_area = np.mean(area)
+    mean_diameter = np.mean(diameter)
 
-    return mean_radius, mean_area
+    return mean_diameter, diameter
 
 
-def histogram_sizes(sizes):
+
+def histogram_sizes(sizes, file_name, np_type):
     """Function for creating histogram of sizes of NP
 
     Args:
         sizes (list): list of NP sizes
     """
 
-    plt.hist(sizes)
-    plt.title('Histogram of sizes of NPs')
-    plt.xlabel('size [px]')
-    plt.ylabel('frequency')
-    plt.savefig('results/histogram.png')
-    plt.clf()
+    file_name = file_name[:-4] + 'hist' + file_name[-4:]
+
+    if np_type == 'nanoparticles':
+        plt.hist(sizes)
+        plt.title('Histogram of sizes of NPs')
+        plt.xlabel('diameter [nm]')
+        plt.ylabel('frequency')
+        plt.savefig(file_name)
+        plt.clf()
+
+    elif np_type == 'nanorods':
+        plt.hist(sizes[0])
+        plt.hist(sizes[1])
+        plt.legend(['major axis', 'minor axis'])
+        plt.title('Histogram of sizes of NRs')
+        plt.xlabel('axis length [nm]')
+        plt.ylabel('frequency')
+        plt.savefig(file_name)
+        plt.clf()
+
+
+
+
+def read_args():
+    """Function for command line arguments
+
+    Returns:
+        dictionary: path to image folder and method of segmentation
+    """
+
+    parser = argparse.ArgumentParser(description='segmentation of NPs',
+                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('-f', '--file', type=str, default='images',
+                        help='path to images (default: images)')
+    parser.add_argument('-m', '--method', type=str, default='watershed',
+                        help='segmentation method (default: watershed)')
+    args = parser.parse_args()
+    config = vars(args)
+    print(config)
+
+    return config
+
+
 
 
 if __name__ == '__main__':
+    config = read_args()
 
-    input_description = load_inputs(
-        '/home/monika/Desktop/project/Nanoparticles_app/images',
-        '/home/monika/Desktop/project/Nanoparticles_app/images/scales.json')
+    input_description = load_inputs(config['file'])
+    method = config['method']
+
+    if method != ('watershed' or 'hough'):
+        raise Exception('unknown method')
+
+    images = []
+    names = []
 
     for image in input_description:
         scale = int(input_description[image][0])
@@ -322,16 +430,26 @@ if __name__ == '__main__':
 
         img_filtered, pixel_size = filtering_img(
             img_raw, scale, np_type, pixel_size)
-        print(pixel_size)
 
-        labels, distance = watershed_transform(img_filtered)
+        if method == 'watershed':
 
-        file_name = saving_img(labels)
-
-        avg = calculation_watershed(labels, pixel_size,
+            img, distance = watershed_transform(img_filtered)
+            diameter, sizes = calculation_watershed(img, pixel_size,
                                                     np_type)
 
-        if np_type == 'nanoparticles':
-            img, circles = hough_segmentation(img_filtered, np_type)
-            radius, area = calcultaion_hough_transform(circles,
-                                                pixel_size, np_type)
+        elif method == 'hough':
+            if np_type == 'nanoparticles':
+                img, circles = hough_segmentation(img_filtered, pixel_size)
+                diameter, sizes = calcultaion_hough_transform(circles,
+                                                    pixel_size, np_type)
+            elif np_type == 'nanorods':
+                pass
+
+        images.append(img)
+        names.append(img_path)
+
+        file_name = saving_img(img, img_path)
+        histogram_sizes(sizes, file_name, np_type)
+
+        print('saving into:', file_name)
+    ploting_img(images, names, method)
---
+++ tryouts/ellipse_detection.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+
+from skimage import data, color, img_as_ubyte
+from skimage.feature import canny
+from skimage.transform import hough_ellipse
+from skimage.draw import ellipse_perimeter
+
+# Load picture, convert to grayscale and detect edges
+image_rgb = data.coffee()[0:220, 160:420]
+image_gray = color.rgb2gray(image_rgb)
+edges = canny(image_gray, sigma=2.0,
+              low_threshold=0.55, high_threshold=0.8)
+
+# Perform a Hough Transform
+# The accuracy corresponds to the bin size of a major axis.
+# The value is chosen in order to get a single high accumulator.
+# The threshold eliminates low accumulators
+result = hough_ellipse(edges, accuracy=50, threshold=50,
+                       min_size=10, max_size=100)
+result.sort(order='accumulator')
+
+# Estimated parameters for the ellipse
+best = list(result[-1])
+yc, xc, a, b = [int(round(x)) for x in best[1:5]]
+orientation = best[5]
+
+# Draw the ellipse on the original image
+cy, cx = ellipse_perimeter(yc, xc, a, b, orientation)
+image_rgb[cy, cx] = (0, 0, 255)
+# Draw the edge (white) and the resulting ellipse (red)
+edges = color.gray2rgb(img_as_ubyte(edges))
+edges[cy, cx] = (250, 0, 0)
+
+fig2, (ax1, ax2) = plt.subplots(ncols=2, nrows=1, figsize=(8, 4),
+                                sharex=True, sharey=True)
+
+ax1.set_title('Original picture')
+ax1.imshow(image_rgb)
+
+ax2.set_title('Edge (white) and result (red)')
+ax2.imshow(edges)
+
+plt.show()
